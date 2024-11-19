03f7411b26367da76110cd25485e7999448a8b53
vodoem <prosto_duz@mail.ru>
2024-11-19 23:19:24 +0300

    Был добавлен record Rect который определяет область рендера

---
+++ src/main/java/backend/academy/flame_fractal/domain/Rect.java
@@ -0,0 +1,8 @@
+package backend.academy.flame_fractal.domain;
+
+public record Rect(double x, double y, double width, double height) {
+    public boolean contains(Point point) {
+        return point.x() >= x && point.x() <= x + width &&
+            point.y() >= y && point.y() <= y + height;
+    }
+}
