a3fbae8cff6b42ad9e08f2dc6c6540f97c9046fc
vodoem <prosto_duz@mail.ru>
2024-11-19 23:14:43 +0300

    Был добавлен record FractalImage и реализованы методы contains, pixel и setPixel которые предоставляют удобный интерфейс для работы с пикселями

---
+++ src/main/java/backend/academy/flame_fractal/domain/FractalImage.java
@@ -0,0 +1,29 @@
+package backend.academy.flame_fractal.domain;
+
+import java.util.Arrays;
+
+public record FractalImage(Pixel[] data, int width, int height) {
+    public static FractalImage create(int width, int height) {
+        Pixel[] data = new Pixel[width * height];
+        Arrays.fill(data, new Pixel(0, 0, 0, 0));
+        return new FractalImage(data, width, height);
+    }
+
+    public boolean contains(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+
+    public Pixel pixel(int x, int y) {
+        if (!contains(x, y)) {
+            throw new IllegalArgumentException("Координаты выходят за пределы");
+        }
+        return data[y * width + x];
+    }
+
+    public void setPixel(int x, int y, Pixel pixel) {
+        if (!contains(x, y)) {
+            throw new IllegalArgumentException("Координаты выходят за пределы");
+        }
+        data[y * width + x] = pixel;
+    }
+}
